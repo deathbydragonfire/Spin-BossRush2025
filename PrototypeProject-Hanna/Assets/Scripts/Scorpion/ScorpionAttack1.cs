e1551beebf92a21cc732b3e730f244233abf20f4
Erkhan <erkhanpiriyev@gmail.com>
2025-01-12 10:46:23 +0500

    add Health.cs abstract class

---
+++ PrototypeProject-Hanna/Assets/Scripts/Scorpion/ScorpionAttack1.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ScorpionAttack1 : StateMachineBehaviour
+{ 
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+{
+
+}
+
+override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+{
+
+}
+
+override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+{
+
+}
+}
